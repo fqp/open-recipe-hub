5781700ea155aae01c00141aeb6bb99a9cd50020
Wesley van der Sanden <wesley.vandersanden@coosto.com>
2016-09-16 11:22:47 +0200

    Implement VueRouter and remove shitty example code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+// import views
+import HomeView from './components/Home'
+import RecipeView from './components/Recipe'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+export var router = new VueRouter()
+
+router.map({
+  '/home': {
+    component: HomeView
+  },
+  '/recipe': {
+    component: RecipeView
+  }
 })
+
+router.redirect({
+  '*': '/home'
+})
+
+router.start(App, 'app')
