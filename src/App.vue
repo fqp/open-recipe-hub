39abecba5963fd3eec439c0da757b7a83738d3f6
Wesley van der Sanden <wesley.vandersanden@coosto.com>
2016-09-16 13:12:17 +0200

    Add headerbar, vue-strap and bootstrap css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
+  <header-bar></header-bar>
   <div id="app">
+    <alert :show.sync="showRight" placement="top-right" duration="3000" type="success" width="400px" dismissable>
+      <span class="icon-ok-circled alert-icon-float-left"></span>
+      <strong>Well Done!</strong>
+      <p>You successfully read this important alert message.</p>
+    </alert>
+    <button @click="showAlert">Show</button>
     <a v-link="{ path: '/home' }">Home</a>
     <a v-link="{ path: '/recipe' }">Recipe</a>
     <router-view></router-view>
@@ -7,12 +14,30 @@
 </template>
 
 <script>
+import HeaderBar from './components/HeaderBar'
+import { alert } from 'vue-strap'
+
 export default {
   components: {
+    HeaderBar,
+    alert
+  },
+  data () {
+    return {
+      showRight: false
+    }
+  },
+  methods: {
+    showAlert: function () {
+      this.showRight = true
+    }
   }
 }
 </script>
 
 <style lang="scss">
-
+* {
+  box-sizing: border-box;
+  outline: 0;
+}
 </style>
